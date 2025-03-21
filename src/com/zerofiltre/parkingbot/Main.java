72c2d5198d5cd7d1fc8c143dc328af3e4df52c19
Cabrel MBIAPO <idrisscabrel53@gmail.com>
2025-03-21 22:39:55 +0100

    p1c2d2 OK

--- src/com/zerofiltre/parkingbot/Main.java
+++ src/com/zerofiltre/parkingbot/Main.java
@@ -1,8 +1,20 @@
 package com.zerofiltre.parkingbot;
 
-public class Main {
+public class  Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int length = 180;
+        int width = 120;
+        int surface = length * width;
+        int perimeter = (length + width) * 2;
+        int difference = length - width;
+        float ratio = (float) width / length;
+
+        System.out.println("La surface au sol de la voiture est de: "+ surface);
+        System.out.println("Le périmètre au sol de la voiture est de: "+ perimeter);
+        System.out.println("La difference entre la longueur et la largeur est de: "+ difference);
+        System.out.println("Le ratio entre la largeur et la longueur est de: "+ ratio);
+
+
     }
 }
