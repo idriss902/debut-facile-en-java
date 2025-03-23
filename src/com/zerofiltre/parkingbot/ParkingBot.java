9ab40fc00521a0cf39554eb9f7f6bd4f3cdece71
Cabrel MBIAPO <idrisscabrel53@gmail.com>
2025-03-23 05:05:39 +0100

    p1c4 OK

--- src/com/zerofiltre/parkingbot/ParkingBot.java
+++ src/com/zerofiltre/parkingbot/ParkingBot.java
@@ -1,7 +1,14 @@
 package com.zerofiltre.parkingbot;
 
+import com.zerofiltre.parkingbot.service.ParkingService;
+
 public class ParkingBot {
 
+  /**
+   * ceci est une variable globale
+   */
+  static String completion = "des équipes du parking Zerofiltre";
+  static ParkingService parkingService = new ParkingService();
   /**
    * Ceci est la méthode Main
    *
@@ -17,11 +24,17 @@ public class ParkingBot {
    * Cette méthode permet de dire Hello
    */
   private static void sayHello() {
-    String welcomeSentence = "Hello, recevez la bienvenue ";
+    String welcomeSentence = "Hello, recevez la bienvenue " + completion;
     System.out.println(welcomeSentence);
+
+    String parkService = parkingService.parking;
+    String washingService = parkingService.washing;
+
+    System.out.println("Nous proposons les services suivants : " + parkService + " ET " + washingService);
   }
 
   private static void sayBye() {
-    System.out.println("Recevez les aurevoirs");
+    System.out.println("Recevez les aurevoirs " + completion);
+
   }
 }
---
+++ src/com/zerofiltre/parkingbot/service/ParkingService.java
@@ -0,0 +1,7 @@
+package com.zerofiltre.parkingbot.service;
+
+public class ParkingService {
+
+    public  String parking = "Parking et surveillance de véhicules";
+    public String washing = "Nettoyage de véhicules";
+}
