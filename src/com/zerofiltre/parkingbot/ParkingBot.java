9a99610964d213583e393d876d468eee313f6d24
Cabrel MBIAPO <idrisscabrel53@gmail.com>
2025-03-24 06:20:11 +0100

    p1c10 OK

--- src/com/zerofiltre/parkingbot/ParkingBot.java
+++ src/com/zerofiltre/parkingbot/ParkingBot.java
@@ -15,6 +15,14 @@ public class ParkingBot {
    */
   public static void main(String[] args) {
     processVehicles();
+//
+//    int a =10;
+//    int b = a;
+//    System.out.println("a " + a);
+//    System.out.println("b " + b);
+//    a=15;
+//    System.out.println("a " + a);
+//    System.out.println("b " + b);
   }
 
   private static void processVehicles() {
@@ -24,8 +32,9 @@ public class ParkingBot {
     Ticket vehicleTicket = parkingService.processIncomingVehicle(vehicle);
     System.out.println(vehicleTicket);
 
+    parkingService.processExitingVehicle(vehicleTicket);
 
-
+    System.out.println(vehicleTicket);
   }
 
 
